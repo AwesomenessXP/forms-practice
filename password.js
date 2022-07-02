6f38ec3305d4fe686101520e7ddf0fcca70ab011
HaskMoney <heartstring85@gmail.com>
2022-07-01 23:26:05 -0700

    styled forms

--- password.js
+++ password.js
@@ -2,12 +2,16 @@ const userName = document.getElementById("user");
 const passWord = document.getElementById('password');
 const age = document.getElementById('age');
 const submitBtn = document.getElementById('submit-btn');
+const email = document.getElementById('email');
+
 const userNameOutput = document.getElementById('name-output');
 const passWordOutput = document.getElementById('pass-output');
 const ageOutput = document.getElementById('age-output');
+const emailOutput = document.getElementById('email-output');
 
 submitBtn.addEventListener('click', (event) => {
     userNameOutput.textContent = `Hello, ${userName.value}`;
     passWordOutput.textContent= `Your password is: ${passWord.value}`;
     ageOutput.textContent = `Your age is: ${age.value}`;
+    emailOutput.textContent = `Your email is: ${email.value}`;
 });
